afdb7499c20578572e41b85f6ee2ecc1a65add60
AlejandroSR25 <72152408+AlejandroSR25@users.noreply.github.com>
2021-11-19 13:26:36 -0500

    Add files via upload

---
+++ Honda.java
@@ -0,0 +1,44 @@
+public class Honda extends Moto implements IMotoGasolina{
+  int kilometraje;
+  int gasolina;
+
+
+  public Honda(){
+    this.kilometraje=0;
+    this.gasolina=100;
+  }
+
+  public Honda(int kilometraje, int gasolina){
+    this.kilometraje=kilometraje;
+    this.gasolina=gasolina;
+  }
+
+  @Override
+  public int mostrarKilometraje(){
+    return this.kilometraje;
+  }
+
+  @Override
+  public void conducir(){
+    if(this.gasolina>0){
+      this.kilometraje+=10;
+      this.gasolina-=10;
+    }
+    else{
+      System.out.println("No hay suficiente combustible, ingrese la cantidad a tanquear\n");
+      int cantidad_tanqueo = Integer.parseInt(Leer.leer());
+      tanquear(cantidad_tanqueo);
+      System.out.println("Tanqueo exitoso, se procedera a conducir el vehiculo");
+      conducir();
+    }
+  }
+  
+  @Override
+  public void tanquear(int cantidad_tanqueo){
+    this.gasolina += cantidad_tanqueo;
+    System.out.println("XD");
+    if(this.gasolina>100){
+      this.gasolina=100;
+    }
+  }
+}
