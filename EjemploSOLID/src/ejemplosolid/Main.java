a1ec374cd2f938610ccc23c8edaadbdc5ece8058
AlejandroSR25 <72152408+AlejandroSR25@users.noreply.github.com>
2021-11-24 12:53:30 -0500

    Update Main.java

--- EjemploSOLID/src/ejemplosolid/Main.java
+++ EjemploSOLID/src/ejemplosolid/Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     
+    //Se crea array de tipo Moto donde se añaden objetos de clases hijas de Moto.
     Moto[] arrayMotos = {
       new Honda(),
       new Kawasaki(10, 90),
@@ -8,6 +9,7 @@ class Main {
       new Starker()
     };
     
+    //For each para recorrer el arreglo de motos y llamar los metodos de las clases.
     for(Moto moto: arrayMotos){
       moto.conducir();
       System.out.println(moto.mostrarKilometraje());
