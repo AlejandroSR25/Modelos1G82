7e877ae1ec992dad713d9abbfc50da75ac2b879c
Cristian-Agudelo <94707477+Cristian-Agudelo@users.noreply.github.com>
2021-11-24 12:55:32 -0500

    Update Moto.java

--- EjemploSOLID/src/ejemplosolid/Moto.java
+++ EjemploSOLID/src/ejemplosolid/Moto.java
@@ -1,6 +1,6 @@
 public abstract class Moto{
   
-  //Metódos abstractos para ser implementados por cada moto
+  //Metódos abstractos para ser implementados por cada moto.
   public abstract int mostrarKilometraje();
   public abstract void conducir();
 }
