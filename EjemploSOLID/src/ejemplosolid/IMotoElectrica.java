01770318271953ebd76619f1735914b9a28d52db
AlejandroSR25 <72152408+AlejandroSR25@users.noreply.github.com>
2021-11-26 08:30:25 -0500

    Update IMotoElectrica.java

--- EjemploSOLID/src/ejemplosolid/IMotoElectrica.java
+++ EjemploSOLID/src/ejemplosolid/IMotoElectrica.java
@@ -1,4 +1,4 @@
-//Interface para motos de tipo electricas
+//Interface para motos de tipo electricas, en esta clase se puede evidenciar el principio de segregacion de interfaces.
 public interface IMotoElectrica{
   public void cargarBateria(int cantidad_carga);
 }
