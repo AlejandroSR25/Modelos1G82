3bd4aa0cbe7b35787ca6e2eb97a5808e0db0c8fb
AlejandroSR25 <72152408+AlejandroSR25@users.noreply.github.com>
2021-11-26 08:30:36 -0500

    Update IMotoGasolina.java

--- EjemploSOLID/src/ejemplosolid/IMotoGasolina.java
+++ EjemploSOLID/src/ejemplosolid/IMotoGasolina.java
@@ -1,4 +1,4 @@
-//Interface para motos con consumo con gasolina
+//Interface para motos con consumo con gasolina, en esta clase se puede evidenciar el principio de segregacion de interfaces.
 public interface IMotoGasolina{
   
   public void tanquear(int cantidad_tanqueo);
