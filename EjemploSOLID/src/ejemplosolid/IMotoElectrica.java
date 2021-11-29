4e80bb99b618c1818602424156f81467ae47c379
sergiosierrra <77708854+sergiosierrra@users.noreply.github.com>
2021-11-29 18:00:15 -0500

    Update IMotoElectrica.java

--- EjemploSOLID/src/ejemplosolid/IMotoElectrica.java
+++ EjemploSOLID/src/ejemplosolid/IMotoElectrica.java
@@ -1,4 +1,7 @@
-//Interface para motos de tipo electricas, en esta clase se puede evidenciar el principio de segregacion de interfaces.
+/*Interface para motos de tipo electricas, en esta clase se puede evidenciar el principio de segregacion de interfaces.
+ * @author: Dylan Solarte, Sergio Sierra, Cristian Agudelo
+ * @version: 29/11/2021/A
+ */
 public interface IMotoElectrica{
   public void cargarBateria(int cantidad_carga);
 }
