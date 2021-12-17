c32947406ee6de82cd433a6932dfc0115aefda66
sergiosierrra <77708854+sergiosierrra@users.noreply.github.com>
2021-12-17 13:16:44 -0500

    Update Personajes.py

--- ProyectoPrimerCorte/Logica/Personajes.py
+++ ProyectoPrimerCorte/Logica/Personajes.py
@@ -4,15 +4,18 @@ from abc import ABC, abstractmethod
 class Personaje(ABC):
     @abstractmethod
     def clone(self):
+        '''Funcion para clonar los personajes'''
         return deepcopy(self)
     
     @abstractmethod
     def avatarGenero(self):
+        '''Funcion para determinar el genero del personaje'''
         pass
 
 
 class Humano(Personaje):
     def __init__(self, genero):
+        '''Funcion para inicializar el humano'''
         self.vida = 100
         self.genero = genero
         self.avatar_path = self.avatarGenero(genero)
@@ -23,44 +26,57 @@ class Humano(Personaje):
 
     #Setters
     def setGenero(self, genero):
+        '''Funcion set del atributo'''
         self.genero = genero
 
     def setAvatarPath(self, avatar_path):
+        '''Funcion set del atributo'''
         self.avatar_path = avatar_path
 
     def setArmaPath(self, arma_path):
+        '''Funcion set del atributo'''
         self.arma_path = arma_path
     
     def setArmaduraPath(self, armadura_path):
+        '''Funcion set del atributo'''
         self.armadura_path = armadura_path
 
     def setEscudoPath(self, escudo_path):
+        '''Funcion set del atributo'''
         self.escudo_path = escudo_path
 
     def setMonturaPath(self, montura_path):
+        '''Funcion set del atributo'''
         self.montura_path = montura_path
 
     #Getters
 
     def getGenero(self):
+        '''Funcion get del atributo'''
         return self.genero
 
     def getAvatarPath(self):
+        '''Funcion get del atributo'''
         return self.avatar_path
 
     def getArmaPath(self):
+        '''Funcion get del atributo'''
         return self.arma_path
     
     def getArmaduraPath(self):
+        '''Funcion get del atributo'''
         return self.armadura_path
 
     def getEscudoPath(self):
+        '''Funcion get del atributo'''
         return self.escudo_path
 
     def getMonturaPath(self):
+        '''Funcion get del atributo'''
         return self.montura_path    
 
     def avatarGenero(self, genero):
+        '''Funcion para definir la ruta dependiendo del genero'''
         if genero == 'H':
             return "../Imagenes/Humano_H.jpg"
         else:
@@ -69,10 +85,12 @@ class Humano(Personaje):
     #Metodo Clonacion
 
     def clone(self):
+        '''Funcion para clonar'''
         return deepcopy(self)
 
 class Elfo(Personaje):
     def __init__(self, genero):
+        '''Funcion para inicializar el elfo'''
         self.vida = 100
         self.genero = genero
         self.avatar_path = self.avatarGenero(genero)
@@ -83,44 +101,57 @@ class Elfo(Personaje):
 
     #Setters
     def setGenero(self, genero):
+        '''Funcion set para el atributo'''
         self.genero = genero
 
     def setAvatarPath(self, avatar_path):
+        '''Funcion set para el atributo'''
         self.avatar_path = avatar_path
 
     def setArmaPath(self, arma_path):
+        '''Funcion set para el atributo'''
         self.arma_path = arma_path
     
     def setArmaduraPath(self, armadura_path):
+        '''Funcion set para el atributo'''
         self.armadura_path = armadura_path
 
     def setEscudoPath(self, escudo_path):
+        '''Funcion set para el atributo'''
         self.escudo_path = escudo_path
 
     def setMonturaPath(self, montura_path):
+        '''Funcion set para el atributo'''
         self.montura_path = montura_path
 
     #Getters
 
     def getGenero(self):
+        '''Funcion get para el atributo'''
         return self.genero
 
     def getAvatarPath(self):
+        '''Funcion get para el atributo'''
         return self.avatar_path
 
     def getArmaPath(self):
+        '''Funcion get para el atributo'''
         return self.arma_path
     
     def getArmaduraPath(self):
+        '''Funcion get para el atributo'''
         return self.armadura_path
 
     def getEscudoPath(self):
+        '''Funcion get para el atributo'''
         return self.escudo_path
 
     def getMonturaPath(self):
+        '''Funcion get para el atributo'''
         return self.montura_path    
 
     def avatarGenero(self, genero):
+        '''Funcion para definir la ruta dependiendo del genero'''
         if genero == 'H':
             return "../Imagenes/Elfo_H.png"
         else:
@@ -129,10 +160,12 @@ class Elfo(Personaje):
     #Metodo Clonacion
 
     def clone(self):
+        '''Funcion para clonar'''
         return deepcopy(self)
 
 class Enano(Personaje):
     def __init__(self, genero):
+        '''Funcion para inicializar el Enano'''
         self.vida = 100
         self.genero = genero
         self.avatar_path = self.avatarGenero(genero)
@@ -143,44 +176,57 @@ class Enano(Personaje):
 
     #Setters
     def setGenero(self, genero):
+        '''Funcion set del atributo'''
         self.genero = genero
 
     def setAvatarPath(self, avatar_path):
+        '''Funcion set del atributo'''
         self.avatar_path = avatar_path
 
     def setArmaPath(self, arma_path):
+        '''Funcion set del atributo'''
         self.arma_path = arma_path
     
     def setArmaduraPath(self, armadura_path):
+        '''Funcion set del atributo'''
         self.armadura_path = armadura_path
 
     def setEscudoPath(self, escudo_path):
+        '''Funcion set del atributo'''
         self.escudo_path = escudo_path
 
     def setMonturaPath(self, montura_path):
+        '''Funcion set del atributo'''
         self.montura_path = montura_path
 
     #Getters
 
     def getGenero(self):
+        '''Funcion get del atributo'''
         return self.genero
 
     def getAvatarPath(self):
+        '''Funcion get del atributo'''
         return self.avatar_path
 
     def getArmaPath(self):
+        '''Funcion get del atributo'''
         return self.arma_path
     
     def getArmaduraPath(self):
+        '''Funcion get del atributo'''
         return self.armadura_path
 
     def getEscudoPath(self):
+        '''Funcion get del atributo'''
         return self.escudo_path
 
     def getMonturaPath(self):
+        '''Funcion get del atributo'''
         return self.montura_path    
     
     def avatarGenero(self, genero):
+        '''Funcion para definir la ruta dependiendo del genero'''
         if genero == 'H':
             return '../Imagenes/Enano_H.png'
         else:
@@ -189,9 +235,12 @@ class Enano(Personaje):
     #Metodo Clonacion
 
     def clone(self):
+        '''Funcion para clonar'''
         return deepcopy(self)
+    
 class Orco(Personaje):
     def __init__(self, genero):
+        '''Funcion para inicializar el Orco'''
         self.vida = 100
         self.genero = genero
         self.avatar_path = self.avatarGenero(genero)
@@ -202,44 +251,57 @@ class Orco(Personaje):
 
     #Setters
     def setGenero(self, genero):
+        '''Funcion set del atributo'''
         self.genero = genero
 
     def setAvatarPath(self, avatar_path):
+        '''Funcion set del atributo'''
         self.avatar_path = avatar_path
 
     def setArmaPath(self, arma_path):
+        '''Funcion set del atributo'''
         self.arma_path = arma_path
     
     def setArmaduraPath(self, armadura_path):
+        '''Funcion set del atributo'''
         self.armadura_path = armadura_path
 
     def setEscudoPath(self, escudo_path):
+        '''Funcion set del atributo'''
         self.escudo_path = escudo_path
 
     def setMonturaPath(self, montura_path):
+        '''Funcion set del atributo'''
         self.montura_path = montura_path
 
     #Getters
 
     def getGenero(self):
+        '''Funcion get del atributo'''
         return self.genero
 
     def getAvatarPath(self):
+        '''Funcion get del atributo'''
         return self.avatar_path
 
     def getArmaPath(self):
+        '''Funcion get del atributo'''
         return self.arma_path
     
     def getArmaduraPath(self):
+        '''Funcion get del atributo'''
         return self.armadura_path
 
     def getEscudoPath(self):
+        '''Funcion get del atributo'''
         return self.escudo_path
 
     def getMonturaPath(self):
+        '''Funcion get del atributo'''
         return self.montura_path    
 
     def avatarGenero(self, genero):
+        '''Funcion para definir la ruta dependiendo del genero'''
         if genero == 'H':
             return '../Imagenes/Orco_H.png'
         else:
@@ -248,6 +310,7 @@ class Orco(Personaje):
     #Metodo Clonacion
 
     def clone(self):
+        '''Funcion para clonar'''
         return deepcopy(self)
 
 class Hobbit(Personaje):
