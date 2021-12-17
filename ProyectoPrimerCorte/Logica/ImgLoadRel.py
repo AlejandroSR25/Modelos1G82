d65117ac9d6077ea15c52d7d4360cb85cea97f0e
sergiosierrra <77708854+sergiosierrra@users.noreply.github.com>
2021-12-17 12:53:08 -0500

    Update ImgLoadRel.py

--- ProyectoPrimerCorte/Logica/ImgLoadRel.py
+++ ProyectoPrimerCorte/Logica/ImgLoadRel.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageTk
 class ImgRelativeLoad:
     @staticmethod
     def loadRelImages(relativepath, resizex, resizey):
+        '''Funcion busca las imagenes mediante el directorio que se le pasa y despues la retorna'''
         directory_path = os.path.dirname(__file__)
         file_path = os.path.join(directory_path, relativepath)
         img = Image.open(file_path.replace(
