40e46495db6dc3f773439cbbd6eb77e30673f7b1
sergiosierrra <77708854+sergiosierrra@users.noreply.github.com>
2021-12-17 12:52:14 -0500

    Update Creadores.py

--- ProyectoPrimerCorte/Logica/Creadores.py
+++ ProyectoPrimerCorte/Logica/Creadores.py
@@ -3,30 +3,38 @@ from Personajes import *
 
 
 class Creador(ABC):
+'''Clase creador para la fabrica'''
     def crear_personaje(self):
+        '''Funcion de crear los personajes'''
         return self.factory_method()
 
     @abstractmethod
     def factory_method(self):
+        '''Metodo abstracto'''
         pass
 
 
 class CreadorHumano(Creador):
     def factory_method(self, genero):
+        '''Funcion de los humanos'''
         return Humano(genero)
 
 class CreadorElfo(Creador):
     def factory_method(self, genero):
+        '''Funcion de los elfos'''
         return Elfo(genero)
 
 class CreadorEnano(Creador):
     def factory_method(self, genero):
+        '''Funcion de los enanos'''
         return Enano(genero)
 
 class CreadorOrco(Creador):
     def factory_method(self, genero):
+        '''Funcion de los orcos'''
         return Orco(genero)
 
 class CreadorHobbit(Creador):
     def factory_method(self, genero):
+        '''Funcion de los Hobbit'''
         return Hobbit(genero)
