0a128b8d19ed9db20a356f11f7f07d681d4bedf8
AlejandroSR25 <72152408+AlejandroSR25@users.noreply.github.com>
2022-03-23 16:34:55 -0500

    .

---
+++ ProyectoCineJungla/src/proyectocinejungla/ProyectoCineJungla.java
@@ -0,0 +1,17 @@
+package proyectocinejungla;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ProyectoCineJungla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainPage Mp = new MainPage();
+        Mp.setVisible(true);
+    }
+    
+}
